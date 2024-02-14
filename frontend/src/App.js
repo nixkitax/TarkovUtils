e2a9cf15b55a2f5ab1945f26f81509d57ee7ab4b
nixkitax <nicol.eeemanuele@icloud.com>
2024-02-14 04:24:39 +0100

    First commmit, header created

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Header />
+        <h1 className="text-2xl">Benvenuto su My App!</h1>
+        <p>Benvenuto nella mia applicazione React.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
