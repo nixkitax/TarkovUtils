2bf30bb300b2ad845e1282b1f1a2b289a88ac697
nixkitax <nicol.eeemanuele@icloud.com>
2024-02-17 03:42:57 +0200

    update home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <div className="bg-white py-10 px-10 rounded-xl shadow-xl mt-10">
             <div className='text-black'>
-                <h1 className='text-xl font-bold mb-3'> A lot of utils stuff for Tarkov</h1>
+                <h1 className='text-xl font-bold mb-3'> A lot of useful stuff for Tarkov</h1>
                 <h1 className='text-5xl font-bold'> TarkovsUtils </h1>
             </div>
         </div>
